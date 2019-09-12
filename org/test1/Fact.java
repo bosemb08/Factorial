6edc559abdbc7cd3396b2f4a52709eee8cca2647
bosemb08 <bosemb08@gmail.com>
2019-09-12 13:07:03 +0530

    Factorial

---
+++ org/test1/Fact.java
@@ -0,0 +1,15 @@
+package org.test1;
+
+public class Fact {
+	
+	public static void main(String[] args) {
+		
+		int count=1;
+		for (int i = 1; i <= 5; i++) {
+			count=count*i;
+			
+		}
+		System.out.println(count);
+	}
+
+}
